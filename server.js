aae2491f36ca8b72a2c760ba45a8be7f2218c030
GuilhermeSerafim <guilerstudies@gmail.com>
2024-11-18 14:22:39 -0300

    Criação do servidor Express com rota /api

--- server.js
+++ server.js
@@ -2,10 +2,18 @@ import express from "express";
 
 const app = express();
 app.listen(3000, () => {
-    console.log("Serv listening"); 
-}); 
+    console.log("Serv listening");
+});
 
 app.get("/api", (req, res) => {
     res.status(200).send("Hello World");
+});
 
+app.get("/livro", (req, res) => {
+    res.status(200).send({
+        "titulo": "O Senhor dos Anéis",
+        "autor": "J.R.R. Tolkien",
+        "ano": 1954,
+        "genero": "Fantasia"
+    });
 });
