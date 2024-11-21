607441d3fec65342744bd6dc4639baa07a2cec8f
GuilhermeSerafim <guilerstudies@gmail.com>
2024-11-21 15:54:26 -0300

    Pegando coleção do banco de dados posts e implementando na api

--- server.js
+++ server.js
@@ -1,28 +1,6 @@
 import express from "express";
 import conectarAoBanco from "./src/config/dbConfig.js";
-await conectarAoBanco(process.env.STRING_CONEXAO)
-
-// Mock
-const posts = [
-    {
-        id: 1,
-        desc: "Foto teste",
-        img: "https://placecats.com/millie/300/150",
-        alt: "Descrição da imagem"
-    },
-    {
-        id: 2,
-        desc: "Paisagem incrível!",
-        img: "https://placecats.com/millie/300/150/200/300",
-        alt: "Descrição da imagem"
-    },
-    {
-        id: 3,
-        desc: "Meu novo projeto!",
-        img: "https://placecats.com/millie/300/150",
-        alt: "Descrição da imagem"
-    },
-];
+const conexao = await conectarAoBanco(process.env.STRING_CONEXAO)
 
 const app = express();
 app.use(express.json());
@@ -31,6 +9,15 @@ const buscarPostPorId = (id) => posts.find((post) => post.id === Number(id));
 
 app.listen(3000, () => (console.log("Serv listening")));
 
-app.get("/posts", (req, res) => (res.status(200).json(posts)));
+
+const getAllPosts = () => {
+    const db = conexao.db("imersao-instabyte");    
+    const colecao = db.collection("posts");
+    return colecao.find().toArray();
+}
+app.get("/posts", async (req, res) => {
+    const posts = await getAllPosts();
+    return res.status(200).json(posts);
+});
 
 app.get("/posts/:id", (req, res) => (res.status(200).json(buscarPostPorId(req.params.id)))); 
