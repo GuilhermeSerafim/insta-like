c8b9c8138903da74b8ac3b2c33912ee4b4e60c8a
GuilhermeSerafim <guilerstudies@gmail.com>
2024-11-21 15:00:46 -0300

    Criando variavel de ambiente, conectando com banco e usando string de conexão

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 import express from "express";
+import conectarAoBanco from "./src/config/dbConfig.js";
+await conectarAoBanco(process.env.STRING_CONEXAO)
 
 // Mock
 const posts = [
@@ -6,35 +8,29 @@ const posts = [
         id: 1,
         desc: "Foto teste",
         img: "https://placecats.com/millie/300/150",
-        data: "2023-11-20",
-        autor: "João Silva",
-        likes: 10
+        alt: "Descrição da imagem"
     },
     {
         id: 2,
         desc: "Paisagem incrível!",
         img: "https://placecats.com/millie/300/150/200/300",
-        data: "2023-11-19",
-        autor: "Ana Maria",
-        likes: 25
+        alt: "Descrição da imagem"
     },
     {
         id: 3,
         desc: "Meu novo projeto!",
         img: "https://placecats.com/millie/300/150",
-        data: "2023-11-18",
-        autor: "Pedro Santos",
-        likes: 15
+        alt: "Descrição da imagem"
     },
 ];
 
 const app = express();
 app.use(express.json());
 
+const buscarPostPorId = (id) => posts.find((post) => post.id === Number(id));
+
 app.listen(3000, () => (console.log("Serv listening")));
 
 app.get("/posts", (req, res) => (res.status(200).json(posts)));
-
-const buscarPostPorId = (id) => posts.find((post) => post.id === Number(id));
 
 app.get("/posts/:id", (req, res) => (res.status(200).json(buscarPostPorId(req.params.id)))); 
