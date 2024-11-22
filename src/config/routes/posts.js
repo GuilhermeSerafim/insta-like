51f7dfe6f0c41c5644f4bea3c8c5433fccb731cb
GuilhermeSerafim <guilerstudies@gmail.com>
2024-11-21 21:33:46 -0300

    Adicionando configurações necessárias, para inputar url da msg através do upload no windows

--- src/config/routes/posts.js
+++ src/config/routes/posts.js
@@ -1,9 +1,22 @@
 import express from "express";
-import { listarPosts, postarNovoPost } from "../controller/posts.js";
+import multer from "multer";
+import { listarPosts, postarNovoPost, uploadImg } from "../controller/posts.js";
+
+// Configurações windows upload da imagem
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploads/');
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.originalname);
+    }
+});
+const upload = multer({ dest: "./uploads", storage });
 
 const routes = (app) => {
-    app.use(express.json());    
+    app.use(express.json());
     app.get("/posts", listarPosts);
     app.post("/posts", postarNovoPost);
+    app.post("/upload", upload.single("imagem"), uploadImg); // Single é um middleware que diz que é um arquivo por vez
 }
 export default routes;
